4676c072688bfc8e4e57c7a1a7403dfaf0a3bbf5
goodluck-md <166232237+goodluck-md@users.noreply.github.com>
2024-10-03 19:27:46 +0100

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,11 @@
+// OBEY COMMANDMENTS
+
+global.prefa = ['','!','.',',','🐤','🗿'] //NOT CHANGE
+global.owner = ['2348068831585'] //OWNER
+global.botname = 'Finix 444' //BOT NAME
+global.versisc = "4.4.4" //NOT CHANGE
+global.baileys1 = require('@whiskeysockets/baileys') //NOT CHANGE
+global.creator = "Tama_Ultraa" //NOT CHANGE
+global.packname = "Sticker By" //OPSIONAL
+global.author = "Tama_Ultraa" //OPSIONAL
+global.simbol = "✇" // SIMBOL MENU
